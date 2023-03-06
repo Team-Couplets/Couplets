68ae382fa6dddeb3ae49cb4fa131d98fd8158d09
Santi Lorenzo <santirlorenzo@gmail.com>
2023-03-06 18:55:49 -0500

    app navbar and pages

---
+++ client/components/Message.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+
+function Message() {
+  return (
+    <View>
+      <Text>message</Text>
+    </View>
+  );
+}
+
+export default Message;
---
+++ client/components/Profile.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+
+function Profile() {
+  return (
+    <View>
+      <Text>profile</Text>
+    </View>
+  );
+}
+
+export default Profile;
