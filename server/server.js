7c3e5e09b8386563ec3da948c85d03cddf25f68b
JordanMcLuvin <jcepeda94@gmail.com>
2023-03-06 15:08:43 -0500

    Added express with routing and basic universal error handler

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+app.use(express.static("public"));
+
+const api = require(__dirname + '/routes/api.js');
+
+app.use("/", api);
+
+
+app.use("*", (req, res) => {
+  res.status(404).json("Page not found");
+})
+
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).json('internal server error');
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+})
